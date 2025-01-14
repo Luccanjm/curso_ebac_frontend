3bbf2af6c6262508ef3ae89ec408a87bacd8af34
Luccanjm <lucnjm@gmail.com>
2025-01-14 18:48:17 -0300

    add all files

--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-function validarFormulario(event) {
-    // Previne o envio do formulário
-    event.preventDefault();
+$('form button').click(function(e) {
+    e.preventDefault();
+    const tarefa = $('#nova-tarefa').val().trim();
 
-    // Obtém os valores dos campos A e B
-    var campoA = parseFloat(document.getElementById('campoA').value);
-    var campoB = parseFloat(document.getElementById('campoB').value);
-
-    // Obtém o elemento da mensagem
-    var mensagemDiv = document.getElementById('mensagem');
-
-    // Verifica se o número B é maior que o número A
-    if (campoB > campoA) {
-        // Exibe uma mensagem positiva
-        mensagemDiv.innerHTML = "Formulário válido! O número B é maior que o número A.";
-        mensagemDiv.className = "mensagem-sucesso";
-    } else {
-        // Exibe uma mensagem negativa
-        mensagemDiv.innerHTML = "Formulário inválido! O número B deve ser maior que o número A.";
-        mensagemDiv.className = "mensagem-erro";
+    if(tarefa !== ""){
+        $("#lista-tarefas").append(`<li>${tarefa}</li>`);
+        $("#nova-tarefa").val("");
     }
-}
+});
+
+$("#lista-tarefas").on("click", "li", function(){
+    $(this).toggleClass("line-through");
+})
