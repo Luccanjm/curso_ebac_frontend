eca5b3cf6230d238ca80dff1f4eab893ac7f3413
Lucca Martinelli <lucnjm@gmail.com>
2025-02-05 19:32:57 -0300

    add gulp exercises file

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require ('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify =  require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+function comprimeImagens(){
+    return gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJavaScript(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+function compilaSass(){
+    return gulp.src('./src/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function funcaoPadrao(){
+    console.log("Executando");
+}
+
+exports.default = function(){
+    gulp.watch('./src/styles/*.scss', {ignoreInitial: false}, gulp.series(compilaSass));
+    gulp.watch('./src/scripts/*.js', {ignoreInitial: false}, gulp.series(comprimeJavaScript));
+    gulp.watch('./src/images/*', {ignoreInitial: false}, gulp.series(comprimeImagens));
+    
+}
