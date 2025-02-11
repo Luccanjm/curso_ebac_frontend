66ddfedd009b8b48103590320c2fe9240e4a1e85
Luccanjm <lucnjm@gmail.com>
2025-02-11 19:42:12 -0300

    add files

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
+            development:{
+                files:{
+                    'main.css' : 'main.less'
+                }
+            }
+        },
+        concurrent:{
+            target: ['less']
+        },
+        uglify:{
+            uglify: {
+                target:{
+                   files:{
+                    'main.min.js' : 'main.js'
+                   } 
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['less', 'uglify'])
+
+}
---
+++ main.js
@@ -0,0 +1,1 @@
+nome = document.getElementById('nome');
